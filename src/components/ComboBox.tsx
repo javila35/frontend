538ab6159b5283a2a0179df52f3674147d587ad6
Joe Avila <josepharthuravila@gmail.com>
2025-02-19 16:24:58 -0800

    feat: combobox progress

--- src/components/ComboBox.tsx
+++ src/components/ComboBox.tsx
@@ -1,18 +1,52 @@
-type Option = {
+import { useEffect, useState } from "react";
+
+export type ComboBoxOption = {
   id: number;
   name: string;
 };
 
 type ComboBoxProps = {
-  options: Option[];
-  onClick: (id: number) => void;
+  filterValue: string;
+  options: ComboBoxOption[];
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onNewResourceClick: (resourceName: string) => void;
+  onResourceClick: (resourceId: number) => void;
+  resourceType: string;
 };
 
-export const ComboBox = ({ onClick, options }: ComboBoxProps) => {
+export const ComboBox = ({
+  filterValue,
+  onChange,
+  onResourceClick,
+  onNewResourceClick,
+  options,
+  resourceType,
+}: ComboBoxProps) => {
+  const [filteredOptions, setFilteredOptions] = useState<ComboBoxOption[]>([
+    ...options.slice(0, 5),
+  ]);
+
+  const filterOptions = () =>
+    setFilteredOptions([
+      ...options
+        .filter((option) => option.name.includes(filterValue))
+        .slice(0, 5),
+    ]);
+
+  useEffect(() => {
+    if (filterValue.length >= 1) {
+      filterOptions();
+    } else {
+      setFilteredOptions([...options.slice(0, 5)]);
+    }
+  }, [filterValue]);
+
   return (
     <div className="group mr-4 w-full">
       <div id="combo-box" className="relative">
         <input
+          value={filterValue}
+          onChange={onChange}
           type="text"
           role="combobox"
           id="combo-input"
@@ -20,7 +54,7 @@ export const ComboBox = ({ onClick, options }: ComboBoxProps) => {
           aria-autocomplete="list"
           aria-activedescendant=""
           aria-controls="combo-menu"
-          placeholder="Select a player"
+          placeholder={`Select a ${resourceType}`}
           className="border-norway-500 mr-4 h-12 w-full border-b text-xl font-medium tracking-wider focus-visible:border-b-2 focus-visible:outline-0"
         />
       </div>
@@ -31,16 +65,22 @@ export const ComboBox = ({ onClick, options }: ComboBoxProps) => {
         role="listbox"
         aria-label="combo box options"
       >
-        {options.map(({ id, name }) => (
+        {filteredOptions.map(({ id, name }) => (
           <li
             id={`option-${id}`}
-            className="text-base-600 hover:bg-base-50 flex h-12 cursor-pointer items-center gap-4 p-3 text-sm hover:text-norway-800"
+            className="text-base-600 hover:bg-base-50 hover:text-norway-800 flex h-12 cursor-pointer items-center gap-4 p-3 text-sm"
             role="option"
             aria-selected="false"
           >
             {name}
           </li>
         ))}
+        {!filteredOptions.length && (
+          <li
+            className="text-base-600 p-3"
+            onClick={() => onNewResourceClick(filterValue)}
+          >{`Add new ${resourceType}`}</li>
+        )}
       </ul>
     </div> // end of component
   );
--- src/pages/Seasons/SeasonPage.tsx
+++ src/pages/Seasons/SeasonPage.tsx
@@ -1,48 +1,104 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useParams } from "react-router";
-import { getSeasonById } from "../../api";
-import { Season } from "../../types";
+import { addPlayerToSeason, getAllPlayers, getSeasonById } from "../../api";
+import { Player, Season } from "../../types";
 import { Button } from "../../components/Button";
-import { ComboBox } from "../../components";
+import { ComboBox, ComboBoxOption } from "../../components";
+
+type PlayerMutationParams = {
+  playerId?: number;
+  playerName?: string;
+};
 
 export const SeasonPage = () => {
+  const [input, setInput] = useState<string>("");
   const [selectedPlayerId, setSelectedPlayerId] = useState<number | null>(null);
+  const [playerList, setPlayerList] = useState<ComboBoxOption[]>([]);
+  const [error, setError] = useState<any | null>(null);
   const { id } = useParams();
+  const queryClient = useQueryClient();
   if (!id) {
     return <div>Error! No id in url</div>;
   }
 
-  const { isPending, isError, data, error } = useQuery<Season>({
+  const seasonQuery = useQuery<Season>({
     queryKey: ["season", id],
     queryFn: () => getSeasonById(id),
   });
 
-  if (isPending) {
+  const playersQuery = useQuery<Player[]>({
+    queryKey: ["players"],
+    queryFn: getAllPlayers,
+  });
+
+  const addPlayerMutation = useMutation({
+    mutationFn: ({ playerName = "", playerId }: PlayerMutationParams) =>
+      addPlayerToSeason({
+        seasonId: parseInt(id),
+        playerName: playerName,
+        playerId: playerId,
+      }),
+    onSuccess: (data) => {
+      console.log("success");
+      queryClient.setQueryData(["season", id], data);
+      queryClient.invalidateQueries({ queryKey: ["players"] });
+      setInput("");
+    },
+  });
+
+  // Set errors if encountering them
+  useEffect(() => {
+    if (seasonQuery.isError) {
+      setError({ ...error, getSeasonError: seasonQuery.error });
+    }
+    if (playersQuery.isError) {
+      setError({ ...error, getPlayersError: playersQuery.error });
+    }
+  }, [seasonQuery, playersQuery]);
+
+  // Prep player list for combobox
+  useEffect(() => {
+    if (playersQuery.isSuccess) {
+      setPlayerList(prepPlayersForComboBox(playersQuery.data));
+    }
+  }, [playersQuery.isSuccess, playersQuery.data]);
+
+  if (seasonQuery.isPending || playersQuery.isPending) {
     return <div>Loading...</div>;
   }
 
-  if (isError) {
-    return <span>Error: {error.message}</span>;
+  if (seasonQuery.isError || playersQuery.isError) {
+    {
+      console.error(error);
+    }
+    return <span>Error</span>;
   }
 
   const handleComboboxClick = (id: number) => {
+    setInput(playerList.find((p) => p.id === id)?.name || "");
     setSelectedPlayerId(id);
   };
 
+  const prepPlayersForComboBox = (playerList: Player[]) =>
+    playerList.map((player) => ({ id: player.id, name: player.name }));
+
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="mb-4 text-4xl">{data.name}</h2>
+      <h2 className="mb-4 text-4xl">{seasonQuery.data.name}</h2>
       <h3 className="text-2xl">Players</h3>
-      <ul>{data.players?.map((p) => <li>{p.name}</li>)}</ul>
-      <div className="flex flex-row max-h-12">
+      <ul>{seasonQuery.data.players?.map((p) => <li>{p.name}</li>)}</ul>
+      <div className="flex max-h-12 flex-row">
         {/* <Input /> */}
         <ComboBox
-          options={[
-            { id: 1, name: "joe" },
-            { id: 2, name: "David" },
-          ]}
-          onClick={(id) => handleComboboxClick(id)}
+          filterValue={input}
+          onChange={(e) => setInput(e.target.value)}
+          resourceType="player"
+          options={playerList}
+          onResourceClick={handleComboboxClick}
+          onNewResourceClick={(playerName: string) =>
+            addPlayerMutation.mutate({ playerName })
+          }
         />
         <Button text="Submit" />
       </div>
