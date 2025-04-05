842da85e256c43f48f058f73bd296bf523ab2b24
javila35 <josepharthuravila@gmail.com>
2025-04-05 12:37:15 -0700

    fix: remove heroui

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HeroUIProvider } from "@heroui/react";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HeroUIProvider>
-      <App />
-    </HeroUIProvider>
+    <App />
   </StrictMode>,
 );
